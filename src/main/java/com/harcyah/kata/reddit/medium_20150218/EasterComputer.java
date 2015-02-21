285c4125db09c87e90fd68d7e6b6542ea063aa36
Harcyah <harcyah@gmail.com>
2015-02-21 15:54:32 +0100

    Added reddit medium exercise / 20150218

---
+++ src/main/java/com/harcyah/kata/reddit/medium_20150218/EasterComputer.java
@@ -0,0 +1,22 @@
+package com.harcyah.kata.reddit.medium_20150218;
+
+import org.joda.time.LocalDate;
+
+public class EasterComputer {
+
+	// Butcher/Meess algorithm
+	public LocalDate compute(int year) {
+		int n = year % 19;
+		int c = year / 100;
+		int u = year % 100;
+		int q = (c - ((c + 8) / 25) + 1) / 3;
+		int e = (19 * n + c - (c / 4) - q + 15) % 30;
+		int L = (32 + 2 * (c % 5) + 2 * (u / 4) - e - (u % 4)) % 7;
+		int h = (n + 11 * e + 22 * L) / 451;
+		int r = (e + L - 7 * h + 114);
+		int m = r / 31;
+		int j = r % 31;
+		return new LocalDate(year, m, j + 1);
+	}
+
+}
---
+++ src/test/java/com/harcyah/kata/reddit/medium_20150218/EasterComputerTest.java
@@ -0,0 +1,19 @@
+package com.harcyah.kata.reddit.medium_20150218;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EasterComputerTest {
+
+	@Test
+	public void testCompute() {
+		EasterComputer ec = new EasterComputer();
+		Assert.assertEquals(new LocalDate(2006, 4, 16), ec.compute(2006));
+		Assert.assertEquals(new LocalDate(2014, 4, 20), ec.compute(2014));
+		Assert.assertEquals(new LocalDate(2015, 4, 5), ec.compute(2015));
+		Assert.assertEquals(new LocalDate(2016, 3, 27), ec.compute(2016));
+		Assert.assertEquals(new LocalDate(2017, 4, 16), ec.compute(2017));
+	}
+
+}
