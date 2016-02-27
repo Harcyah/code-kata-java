357982ddee6f751c73c16d5c27c28c2da157d560
Harcyah <harcyah@gmail.com>
2016-02-27 20:50:57 +0100

    Fixed string3 test suite which didnt test the correct package

--- src/test/java/com/harcyah/kata/codingbat/string3/CodingBatString3.java
+++ src/test/java/com/harcyah/kata/codingbat/string3/CodingBatString3.java
@@ -5,7 +5,7 @@ import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
 import org.junit.runner.RunWith;
 
 @RunWith(ClasspathSuite.class)
-@ClassnameFilters({ ".*string2.*" })
+@ClassnameFilters({ ".*string3.*" })
 public class CodingBatString3 {
 
 }
