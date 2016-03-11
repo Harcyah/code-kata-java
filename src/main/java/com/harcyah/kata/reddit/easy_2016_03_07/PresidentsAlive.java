dbea4d225d3251e12dd74f8b3d1129dacb4dc830
Raphaël Bonnet <rbonnet@betomorrow.com>
2016-03-11 12:18:42 +0100

    Done https://www.reddit.com/r/dailyprogrammer/comments/49aatn/20160307_challenge_257_easy_in_what_year_were/

---
+++ src/main/java/com/harcyah/kata/reddit/easy_2016_03_07/PresidentsAlive.java
@@ -0,0 +1,47 @@
+package com.harcyah.kata.reddit.easy_2016_03_07;
+
+import com.google.common.io.Resources;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class PresidentsAlive {
+
+	public int findYearWithMostPresidentsAlive() throws IOException {
+		Map<Integer, Integer> yearsCounter = new HashMap<>();
+		URL resource = Resources.getResource("presidents.csv");
+		List<String> lines = Resources.readLines(resource, StandardCharsets.UTF_8);
+		lines.remove(0);
+
+		for (String line : lines) {
+			String[] tokens = line.split(",");
+			int yearOfBirth = Integer.parseInt(StringUtils.right(tokens[1], 4));
+			int yearOfDeath = 0;
+			if (StringUtils.isBlank(tokens[3])) {
+				yearOfDeath = 2016;
+			} else {
+				yearOfDeath = Integer.parseInt(StringUtils.right(tokens[3], 4));
+			}
+
+			IntStream.range(yearOfBirth, yearOfDeath).boxed().forEach(x -> {
+				yearsCounter.compute(x, (k, v) -> v == null ? 1 : v + 1);
+			});
+		}
+
+		return yearsCounter.entrySet()
+			.stream()
+			.sorted(Map.Entry.<Integer, Integer> comparingByValue().reversed())
+			.limit(1)
+			.findFirst()
+			.get()
+			.getKey();
+	}
+
+}
---
+++ src/test/java/com/harcyah/kata/reddit/easy_2016_03_07/PresidentsAliveTest.java
@@ -0,0 +1,23 @@
+package com.harcyah.kata.reddit.easy_2016_03_07;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class PresidentsAliveTest {
+
+	@Test
+	public void testFindYearWithMostPresidentsAlive() throws IOException {
+		int[] expected = new int[] {
+			1822, 1823, 1824, 1825, 1826,
+			1831, 1833, 1834, 1835, 1836,
+			1837, 1838, 1839, 1840, 1841,
+			1843, 1844, 1845
+		};
+
+		PresidentsAlive pa = new PresidentsAlive();
+		Assertions.assertThat(expected).contains(pa.findYearWithMostPresidentsAlive());
+	}
+
+}
