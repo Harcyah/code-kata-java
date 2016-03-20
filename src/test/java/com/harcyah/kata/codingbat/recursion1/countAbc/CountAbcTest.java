521e7cd57f13dda4929b674dbdfc91f5f80c2224
Harcyah <harcyah@gmail.com>
2016-03-20 18:20:22 +0100

    Some codingbat's recursion1 problems

--- src/test/java/com/harcyah/kata/codingbat/recursion1/countAbc/CountAbcTest.java
+++ src/test/java/com/harcyah/kata/codingbat/recursion1/countAbc/CountAbcTest.java
@@ -1,6 +1,6 @@
 package com.harcyah.kata.codingbat.recursion1.countAbc;
 
-import org.junit.Assert;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class CountAbcTest {
@@ -8,9 +8,18 @@ public class CountAbcTest {
 	@Test
 	public void testCountAbc() {
 		CountAbc countAbc = new CountAbc();
-		Assert.assertTrue(false);
+		Assertions.assertThat(countAbc.countAbc("abc")).isEqualTo(1);
+		Assertions.assertThat(countAbc.countAbc("abcxxabc")).isEqualTo(2);
+		Assertions.assertThat(countAbc.countAbc("abaxxaba")).isEqualTo(2);
+		Assertions.assertThat(countAbc.countAbc("ababc")).isEqualTo(2);
+		Assertions.assertThat(countAbc.countAbc("abxbc")).isEqualTo(0);
+		Assertions.assertThat(countAbc.countAbc("aaabc")).isEqualTo(1);
+		Assertions.assertThat(countAbc.countAbc("hello")).isEqualTo(0);
+		Assertions.assertThat(countAbc.countAbc("")).isEqualTo(0);
+		Assertions.assertThat(countAbc.countAbc("ab")).isEqualTo(0);
+		Assertions.assertThat(countAbc.countAbc("aba")).isEqualTo(1);
+		Assertions.assertThat(countAbc.countAbc("aca")).isEqualTo(0);
+		Assertions.assertThat(countAbc.countAbc("aaa")).isEqualTo(0);
 	}
 
 }
-
-
--- src/test/java/com/harcyah/kata/codingbat/recursion1/strCount/StrCountTest.java
+++ src/test/java/com/harcyah/kata/codingbat/recursion1/strCount/StrCountTest.java
@@ -1,6 +1,6 @@
 package com.harcyah.kata.codingbat.recursion1.strCount;
 
-import org.junit.Assert;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class StrCountTest {
@@ -8,9 +8,20 @@ public class StrCountTest {
 	@Test
 	public void testStrCount() {
 		StrCount strCount = new StrCount();
-		Assert.assertTrue(false);
+		Assertions.assertThat(strCount.strCount("catcowcat", "cat")).isEqualTo(2);
+		Assertions.assertThat(strCount.strCount("catcowcat", "cow")).isEqualTo(1);
+		Assertions.assertThat(strCount.strCount("catcowcat", "dog")).isEqualTo(0);
+		Assertions.assertThat(strCount.strCount("cacatcowcat", "cat")).isEqualTo(2);
+		Assertions.assertThat(strCount.strCount("xyx", "x")).isEqualTo(2);
+		Assertions.assertThat(strCount.strCount("iiiijj", "i")).isEqualTo(4);
+		Assertions.assertThat(strCount.strCount("iiiijj", "ii")).isEqualTo(2);
+		Assertions.assertThat(strCount.strCount("iiiijj", "iii")).isEqualTo(1);
+		Assertions.assertThat(strCount.strCount("iiiijj", "j")).isEqualTo(2);
+		Assertions.assertThat(strCount.strCount("iiiijj", "jj")).isEqualTo(1);
+		Assertions.assertThat(strCount.strCount("aaabababab", "ab")).isEqualTo(4);
+		Assertions.assertThat(strCount.strCount("aaabababab", "aa")).isEqualTo(1);
+		Assertions.assertThat(strCount.strCount("aaabababab", "a")).isEqualTo(6);
+		Assertions.assertThat(strCount.strCount("aaabababab", "b")).isEqualTo(4);
 	}
 
 }
-
-
