3b06cc0f2ad87414d08e3236227cc7bbda14ff4d
Raphaël Bonnet <harcyah@gmail.com>
2016-03-25 12:36:14 +0100

    Done https://www.reddit.com/r/dailyprogrammer/comments/4bc3el/20160321_challenge_259_easy_clarence_the_slow/

---
+++ src/main/java/com/harcyah/kata/reddit/easy_2016_03_21/ClarenceTheSlowTypist.java
@@ -0,0 +1,29 @@
+package com.harcyah.kata.reddit.easy_2016_03_21;
+
+import com.google.common.collect.Maps;
+
+import java.awt.Point;
+import java.util.Map;
+
+public class ClarenceTheSlowTypist {
+
+	private static final Map<Character, Point> POINTS = Maps.newHashMap();
+
+	static {
+		String chars = "123456789.0";
+		for (int i = 0; i < chars.length(); i++) {
+			POINTS.put(chars.charAt(i), new Point(i % 3, Math.floorDiv(i, 3)));
+		}
+	}
+
+	public double solve(String ip) {
+		double length = 0.0;
+		for (int i = 0; i < ip.length() - 1; i++) {
+			char a = ip.charAt(i);
+			char b = ip.charAt(i + 1);
+			length += POINTS.get(a).distance(POINTS.get(b));
+		}
+		return length;
+	}
+
+}
---
+++ src/test/java/com/harcyah/kata/reddit/easy_2016_03_21/ClarenceTheSlowTypistTest.java
@@ -0,0 +1,15 @@
+package com.harcyah.kata.reddit.easy_2016_03_21;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClarenceTheSlowTypistTest {
+
+	protected ClarenceTheSlowTypist typist = new ClarenceTheSlowTypist();
+
+	@Test
+	public void testSolve() {
+		Assert.assertEquals(27.38, typist.solve("219.45.143.143"), 0.01);
+	}
+
+}
