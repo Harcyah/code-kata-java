7ee91db83e7b82576a8897493ab843381700fc2c
Harcyah <harcyah@gmail.com>
2016-03-26 14:35:09 +0100

    ProjectEuler #7, using primes list lookup

--- src/main/java/com/harcyah/kata/projecteuler/Problem_007.java
+++ src/main/java/com/harcyah/kata/projecteuler/Problem_007.java
@@ -1,5 +1,27 @@
 package com.harcyah.kata.projecteuler;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.io.Resources;
+
 public class Problem_007 {
+
+	protected List<Integer> primes;
+
+	public Problem_007() throws NumberFormatException, IOException {
+		primes = Resources.readLines(Resources.getResource("primes-2-to-1299827.txt"), StandardCharsets.UTF_8)
+			.stream()
+			.map(x -> {
+				return Integer.parseInt(x);
+			})
+			.collect(Collectors.toList());
+	}
+
+	public long getNthPrime(int nth) throws NumberFormatException, IOException {
+		return primes.get(nth - 1);
+	}
 
 }
--- src/test/java/com/harcyah/kata/projecteuler/Problem_007Test.java
+++ src/test/java/com/harcyah/kata/projecteuler/Problem_007Test.java
@@ -1,5 +1,17 @@
 package com.harcyah.kata.projecteuler;
 
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem_007Test {
+
+	@Test
+	public void testProblem_007() throws NumberFormatException, IOException {
+		Problem_007 problem_007 = new Problem_007();
+		Assert.assertEquals(13, problem_007.getNthPrime(6));
+		Assert.assertEquals(104743, problem_007.getNthPrime(10001));
+	}
 
 }
