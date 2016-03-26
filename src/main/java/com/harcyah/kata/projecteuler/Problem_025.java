63ef8a9612e61145565b0ddb926c154e38f3c332
Harcyah <harcyah@gmail.com>
2016-03-26 16:45:45 +0100

    Resolved projecteuler's #25

--- src/main/java/com/harcyah/kata/projecteuler/Problem_025.java
+++ src/main/java/com/harcyah/kata/projecteuler/Problem_025.java
@@ -1,5 +1,21 @@
 package com.harcyah.kata.projecteuler;
 
+import java.math.BigInteger;
+
 public class Problem_025 {
+
+	public int getFibonacciSequenceLength(int limit) {
+		BigInteger p = BigInteger.ONE;
+		BigInteger v = BigInteger.ONE;
+		int f = 2;
+		while (String.valueOf(v).length() < limit) {
+			BigInteger p_p = p;
+			BigInteger p_v = v;
+			p = v;
+			v = p_p.add(p_v);
+			f++;
+		}
+		return f;
+	}
 
 }
--- src/test/java/com/harcyah/kata/projecteuler/Problem_025Test.java
+++ src/test/java/com/harcyah/kata/projecteuler/Problem_025Test.java
@@ -1,5 +1,15 @@
 package com.harcyah.kata.projecteuler;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem_025Test {
+
+	@Test
+	public void testProblem_025() {
+		Problem_025 problem_025 = new Problem_025();
+		Assert.assertEquals(12, problem_025.getFibonacciSequenceLength(3));
+		Assert.assertEquals(4782, problem_025.getFibonacciSequenceLength(1000));
+	}
 
 }
