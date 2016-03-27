4a0e986d8791dfa663a2cf137665309e63c06a2e
Harcyah <harcyah@gmail.com>
2016-03-27 20:11:56 +0200

    Done https://www.reddit.com/r/dailyprogrammer/comments/1ystvb/022414_challenge_149_easy_disemvoweler/

---
+++ src/main/java/com/harcyah/kata/reddit/easy_2014_02_24/Disemvoweler.java
@@ -0,0 +1,31 @@
+package com.harcyah.kata.reddit.easy_2014_02_24;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class Disemvoweler {
+
+	private static final char[] VOWELS = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+	public Pair<String, String> disemvowele(String src) {
+		MutablePair<String, String> pair = new MutablePair<>();
+		pair.setLeft("");
+		pair.setRight("");
+		for (int i = 0; i < src.length(); i++) {
+			char c = src.charAt(i);
+			if (!Character.isAlphabetic(c)) {
+				continue;
+			}
+
+			if (Arrays.binarySearch(VOWELS, c) >= 0) {
+				pair.setLeft(pair.getLeft() + c);
+			} else {
+				pair.setRight(pair.getRight() + c);
+			}
+		}
+		return pair;
+	}
+
+}
---
+++ src/test/java/com/harcyah/kata/reddit/easy_2014_02_24/DisemvowelerTest.java
@@ -0,0 +1,25 @@
+package com.harcyah.kata.reddit.easy_2014_02_24;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DisemvowelerTest {
+
+	@Test
+	public void testDisemvoweleStringOne() throws Exception {
+		Disemvoweler disemvoweler = new Disemvoweler();
+		Pair<String, String> pair = disemvoweler.disemvowele("all those who believe in psychokinesis raise my hand");
+		Assert.assertEquals("llthswhblvnpsychknssrsmyhnd", pair.getRight());
+		Assert.assertEquals("aoeoeieeioieiaiea", pair.getLeft());
+	}
+
+	@Test
+	public void testDisemvoweleStringTwo() throws Exception {
+		Disemvoweler disemvoweler = new Disemvoweler();
+		Pair<String, String> pair = disemvoweler.disemvowele("did you hear about the excellent farmer who was outstanding in his field");
+		Assert.assertEquals("ddyhrbtthxcllntfrmrwhwststndngnhsfld", pair.getRight());
+		Assert.assertEquals("ioueaaoueeeeaeoaouaiiiie", pair.getLeft());
+	}
+
+}
