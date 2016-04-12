eafdacc9e66f6fe78334f0e523f71cbfb32ad799
Harcyah <harcyah@gmail.com>
2016-04-12 21:02:48 +0200

    Done euler's 19

--- src/test/java/com/harcyah/kata/projecteuler/Problem_019Test.java
+++ src/test/java/com/harcyah/kata/projecteuler/Problem_019Test.java
@@ -1,5 +1,19 @@
 package com.harcyah.kata.projecteuler;
 
+import org.assertj.core.api.Assertions;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.junit.Test;
+
 public class Problem_019Test {
+
+	@Test
+	public void testCountDays() throws Exception {
+		Problem_019 problem = new Problem_019();
+		DateTime start = new DateTime(1901, 1, 1, 0, 0);
+		DateTime end = new DateTime(2000, 12, 31, 23, 59);
+		int sundaysInTwentiethCentury = problem.countDays(DateTimeConstants.SUNDAY, start, end);
+		Assertions.assertThat(sundaysInTwentiethCentury).isEqualTo(500);
+	}
 
 }
