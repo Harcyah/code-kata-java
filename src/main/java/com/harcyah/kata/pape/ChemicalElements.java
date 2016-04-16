77145a8b25a913f0c08f195970f854bdf85f5794
Harcyah <harcyah@gmail.com>
2016-04-16 15:30:06 +0200

    Made 1 exercise, removed a non interesting one

--- src/main/java/com/harcyah/kata/pape/ChemicalElements.java
+++
@@ -1,8 +0,0 @@
-package com.harcyah.kata.pape;
-
-// http://www.home.hs-karlsruhe.de/~pach0003/informatik_1/aufgaben/en/interfaces.html#chemical_elements_comparable
-// http://www.home.hs-karlsruhe.de/~pach0003/informatik_1/aufgaben/en/misc.html#chemical_elements
-// http://www.home.hs-karlsruhe.de/~pach0003/informatik_1/aufgaben/en/objects.html#chemical_element
-public class ChemicalElements {
-
-}
