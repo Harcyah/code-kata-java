2655b960df6575b46e701afbcf9288d4a22a1653
Raphaël Bonnet <rbonnet@betomorrow.com>
2016-04-22 12:54:37 +0200

    Done https://www.reddit.com/r/dailyprogrammer/comments/4fc896/20160418_challenge_263_easy_calculating_shannon/

---
+++ src/test/java/com/harcyah/kata/reddit/easy_2016_04_18/ShannonEntropyTest.java
@@ -0,0 +1,16 @@
+package com.harcyah.kata.reddit.easy_2016_04_18;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShannonEntropyTest {
+
+	protected ShannonEntropy shannonEntropy = new ShannonEntropy();
+
+	@Test
+	public void testCompute() throws Exception {
+		Assert.assertEquals(1.84644, shannonEntropy.compute("1223334444"), 0.00001);
+		Assert.assertEquals(3.18083, shannonEntropy.compute("Hello, world!"), 0.00001);
+	}
+
+}
