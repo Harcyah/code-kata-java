37064f22786bac4fd64f196ac95f2782c4474be2
Harcyah <harcyah@gmail.com>
2016-12-09 16:42:32 +0100

    Exercism's sumOfMultiples

---
+++ src/main/java/com/harcyah/kata/exercism/sum_of_multiples/SumOfMultiples.java
@@ -0,0 +1,22 @@
+package com.harcyah.kata.exercism.sum_of_multiples;
+
+public class SumOfMultiples {
+
+    public int Sum(int max, int[] values) {
+        int sum = 0;
+        for (int i = 1; i < max; i++) {
+            sum += process(i, values);
+        }
+        return sum;
+    }
+
+    private int process(int i, int[] values) {
+        for (int value : values) {
+            if (i % value == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
