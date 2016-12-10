d6a2647acba29485845ce4e5ddfd70df0bced081
Raph harcyah Bonnet <harcyah@gmail.com>
2016-12-10 18:00:31 +0100

    Removed unused import

--- src/test/java/com/harcyah/kata/exercism/queen_attack/QueenAttackCalculatorTest.java
+++ src/test/java/com/harcyah/kata/exercism/queen_attack/QueenAttackCalculatorTest.java
@@ -1,125 +1,116 @@
 package com.harcyah.kata.exercism.queen_attack;
 
-import com.harcyah.kata.exercism.queen_attack.BoardCoordinate;
-import org.junit.Ignore;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public final class QueenAttackCalculatorTest {
 
-    /*
-     * See https://github.com/junit-team/junit4/wiki/Rules for information on JUnit Rules in general and
-     * ExpectedExceptions in particular.
-     */
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+	/*
+	 * See https://github.com/junit-team/junit4/wiki/Rules for information on
+	 * JUnit Rules in general and ExpectedExceptions in particular.
+	 */
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
-    @Test
-    public void testCoordinateWithNegativeRankNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have positive rank.");
+	@Test
+	public void testCoordinateWithNegativeRankNotAllowed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Coordinate must have positive rank.");
 
-        new BoardCoordinate(-2, 2);
-    }
+		new BoardCoordinate(-2, 2);
+	}
 
-    @Test
-    public void testCoordinateWithRankGreaterThan7NotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have rank <= 7.");
+	@Test
+	public void testCoordinateWithRankGreaterThan7NotAllowed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Coordinate must have rank <= 7.");
 
-        new BoardCoordinate(8, 4);
-    }
+		new BoardCoordinate(8, 4);
+	}
 
-    @Test
-    public void testCoordinateWithNegativeFileNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have positive file.");
+	@Test
+	public void testCoordinateWithNegativeFileNotAllowed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Coordinate must have positive file.");
 
-        new BoardCoordinate(2, -2);
-    }
+		new BoardCoordinate(2, -2);
+	}
 
-    @Test
-    public void testCoordinateWithFileGreaterThan7NotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Coordinate must have file <= 7.");
+	@Test
+	public void testCoordinateWithFileGreaterThan7NotAllowed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Coordinate must have file <= 7.");
 
-        new BoardCoordinate(4, 8);
-    }
+		new BoardCoordinate(4, 8);
+	}
 
-    @Test
-    public void testNullCoordinateNotAllowed() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("You must supply valid board coordinates for both Queens.");
+	@Test
+	public void testNullCoordinateNotAllowed() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("You must supply valid board coordinates for both Queens.");
 
-        new QueenAttackCalculator(null, new BoardCoordinate(0, 7));
-    }
+		new QueenAttackCalculator(null, new BoardCoordinate(0, 7));
+	}
 
-    @Test
-    public void testQueensMustNotOccupyTheSameSquare() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("Queens may not occupy the same board coordinate.");
+	@Test
+	public void testQueensMustNotOccupyTheSameSquare() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("Queens may not occupy the same board coordinate.");
 
-        new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(2, 2));
-    }
+		new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(2, 2));
+	}
 
-    @Test
-    public void testQueensThatDoNotShareRankFileOrDiagonalCannotAttack() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 4), new BoardCoordinate(6, 6));
+	@Test
+	public void testQueensThatDoNotShareRankFileOrDiagonalCannotAttack() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 4), new BoardCoordinate(6, 6));
 
-        assertFalse(calculator.canQueensAttackOneAnother());
-    }
+		assertFalse(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnTheSameRank() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 4), new BoardCoordinate(2, 6));
+	@Test
+	public void testQueensCanAttackOnTheSameRank() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 4), new BoardCoordinate(2, 6));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnTheSameFile() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(4, 5), new BoardCoordinate(2, 5));
+	@Test
+	public void testQueensCanAttackOnTheSameFile() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(4, 5), new BoardCoordinate(2, 5));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnFirstDiagonal() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(0, 4));
+	@Test
+	public void testQueensCanAttackOnFirstDiagonal() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(0, 4));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnSecondDiagonal() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(3, 1));
+	@Test
+	public void testQueensCanAttackOnSecondDiagonal() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(3, 1));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnThirdDiagonal() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(1, 1));
+	@Test
+	public void testQueensCanAttackOnThirdDiagonal() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(1, 1));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
-    @Test
-    public void testQueensCanAttackOnFourthDiagonal() {
-        final QueenAttackCalculator calculator
-                = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(5, 5));
+	@Test
+	public void testQueensCanAttackOnFourthDiagonal() {
+		final QueenAttackCalculator calculator = new QueenAttackCalculator(new BoardCoordinate(2, 2), new BoardCoordinate(5, 5));
 
-        assertTrue(calculator.canQueensAttackOneAnother());
-    }
+		assertTrue(calculator.canQueensAttackOneAnother());
+	}
 
 }
