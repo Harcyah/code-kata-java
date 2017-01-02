1b63f2b96af7ada737c36fe4395fb9b223b856ee
Raphaël Bonnet <rbonnet@betomorrow.com>
2017-01-02 15:16:54 +0100

    Added 1 exercism kata

---
+++ src/test/java/com/harcyah/kata/exercism/pythagorean_triplet/PythagoreanTripletTest.java
@@ -0,0 +1,90 @@
+package com.harcyah.kata.exercism.pythagorean_triplet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PythagoreanTripletTest {
+
+    @Test
+    public void shouldCalculateSum() {
+        PythagoreanTriplet sut = new PythagoreanTriplet(3, 4, 5);
+        final int expected = 12;
+        final int actual = sut.calculateSum();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateProduct() {
+        PythagoreanTriplet sut = new PythagoreanTriplet(3, 4, 5);
+        final long expected = 60l;
+        final long actual = sut.calculateProduct();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsPythagoreanOK() {
+        PythagoreanTriplet sut = new PythagoreanTriplet(3, 4, 5);
+        assertTrue(sut.isPythagorean());
+    }
+
+    @Test
+    public void testIsPythagoreanFail() {
+        PythagoreanTriplet sut = new PythagoreanTriplet(5, 6, 7);
+        assertFalse(sut.isPythagorean());
+    }
+
+    @Test
+    public void shouldMakeTripletsUpToTen() {
+        final List<Long> actual
+                = PythagoreanTriplet
+                        .makeTripletsList()
+                        .withFactorsLessThanOrEqualTo(10)
+                        .build()
+                        .stream()
+                        .map(t -> t.calculateProduct())
+                        .sorted()
+                        .collect(Collectors.toList());
+        final List<Long> expected = Arrays.asList(60l, 480l);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMakeTripletsElevenToTwenty() {
+        final List<Long> actual
+                = PythagoreanTriplet
+                        .makeTripletsList()
+                        .withFactorsGreaterThanOrEqualTo(11)
+                        .withFactorsLessThanOrEqualTo(20)
+                        .build()
+                        .stream()
+                        .map(t -> t.calculateProduct())
+                        .sorted((p1, p2) -> Double.compare(p1, p2))
+                        .collect(Collectors.toList());
+        final List<Long> expected = Arrays.asList(3840l);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMakeTripletsAndFilterOnSum() {
+        final List<Long> actual
+                = PythagoreanTriplet
+                        .makeTripletsList()
+                        .withFactorsLessThanOrEqualTo(100)
+                        .thatSumTo(180)
+                        .build()
+                        .stream()
+                        .map(t -> t.calculateProduct())
+                        .sorted((p1, p2) -> Double.compare(p1, p2))
+                        .collect(Collectors.toList());
+        final List<Long> expected = Arrays.asList(118080l, 168480l, 202500l);
+        assertEquals(expected, actual);
+    }
+
+}
