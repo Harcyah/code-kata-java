65214efebdce96e6978e804ec015c03c637e0735
Raphaël Bonnet <rbonnet@betomorrow.com>
2017-01-02 17:24:42 +0100

    Added 1 exercism kata

---
+++ src/main/java/com/harcyah/kata/exercism/binary_search/BinarySearch.java
@@ -0,0 +1,37 @@
+package com.harcyah.kata.exercism.binary_search;
+
+import java.util.List;
+
+public class BinarySearch<T extends Comparable> {
+
+    private static final int NOT_FOUND = -1;
+
+    private List<T> elements;
+
+    public BinarySearch(List<T> elements) {
+        this.elements = elements;
+    }
+
+    public int indexOf(T needle) {
+        return indexOf(needle, 0, elements.size() - 1);
+    }
+
+    private int indexOf(T needle, int leftIndex, int rightIndex) {
+        if (leftIndex > rightIndex) {
+            return NOT_FOUND;
+        }
+
+        int index = Math.floorDiv(leftIndex + rightIndex, 2);
+        T actual = elements.get(index);
+
+        int result = actual.compareTo(needle);
+        if (result == 0) {
+            return index;
+        } else if (result > 0) {
+            return indexOf(needle, leftIndex, index - 1);
+        } else {
+            return indexOf(needle, index + 1, rightIndex);
+        }
+    }
+
+}
