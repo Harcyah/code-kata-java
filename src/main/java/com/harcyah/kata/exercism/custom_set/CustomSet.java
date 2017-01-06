94cfb54b399fca734161b45ac53e01fe495dd002
Raphaël Bonnet <rbonnet@betomorrow.com>
2017-01-06 14:21:27 +0100

    Added 2 exercism's

---
+++ src/main/java/com/harcyah/kata/exercism/custom_set/CustomSet.java
@@ -0,0 +1,88 @@
+package com.harcyah.kata.exercism.custom_set;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CustomSet<T> {
+
+    private Map<Integer, T> values;
+
+    public CustomSet() {
+        values = new HashMap<>();
+    }
+
+    public CustomSet(List<T> elements) {
+        values = elements.stream().collect(Collectors.toMap(T::hashCode, Function.identity()));
+    }
+
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+
+    public boolean contains(T element) {
+        return values.keySet().contains(element.hashCode());
+    }
+
+    public boolean isDisjoint(CustomSet<T> another) {
+        for (Integer key : this.values.keySet()) {
+            if (another.values.containsKey(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isSubset(CustomSet<T> another) {
+        return this.values.keySet().containsAll(another.values.keySet());
+    }
+
+    public void add(T element) {
+        values.put(element.hashCode(), element);
+    }
+
+    public CustomSet<T> getIntersection(CustomSet<T> another) {
+        CustomSet<T> intersection = new CustomSet<T>();
+        for (Map.Entry<Integer, T> entry : this.values.entrySet()) {
+            if (another.values.containsKey(entry.getKey())) {
+                intersection.values.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return intersection;
+    }
+
+    public CustomSet<T> getDifference(CustomSet<T> another) {
+        CustomSet<T> union = new CustomSet<T>();
+        union.values.putAll(this.values);
+        union.values.keySet().removeAll(another.values.keySet());
+        return union;
+    }
+
+    public CustomSet<T> getUnion(CustomSet<T> another) {
+        CustomSet<T> union = new CustomSet<T>();
+        union.values.putAll(this.values);
+        union.values.putAll(another.values);
+        return union;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CustomSet<?> other = (CustomSet<?>) o;
+        return other.values.equals(this.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return values.hashCode();
+    }
+}
---
+++ src/main/java/com/harcyah/kata/exercism/wordy/WordProblemSolver.java
@@ -0,0 +1,105 @@
+package com.harcyah.kata.exercism.wordy;
+
+final class WordProblemSolver {
+
+    private static final String PREFIX = "What is ";
+    private static final String SUFFIX = "?";
+
+    private interface Applicable {
+
+        int applyOn(int original, int value);
+
+    }
+
+    private enum Operator implements Applicable {
+        PLUS("plus") {
+            @Override
+            public int applyOn(int original, int value) {
+                return original + value;
+            }
+        },
+        MINUS("minus") {
+            @Override
+            public int applyOn(int original, int value) {
+                return original - value;
+            }
+        },
+        MULTIPLY("multiplied by") {
+            @Override
+            public int applyOn(int original, int value) {
+                return original * value;
+            }
+        },
+        DIVIDE("divided by") {
+            @Override
+            public int applyOn(int original, int value) {
+                return original / value;
+            }
+        };
+
+        private final String english;
+
+        Operator(String english) {
+            this.english = english;
+        }
+
+        public String getEnglish() {
+            return english;
+        }
+    }
+
+    public int solve(String words) {
+        if (!words.startsWith(PREFIX)) {
+            throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+        }
+        if (!words.endsWith(SUFFIX)) {
+            throw new IllegalArgumentException("Be polite!");
+        }
+
+        String contents = words.substring(PREFIX.length(), words.length() - SUFFIX.length());
+        int firstSpaceIndex = contents.indexOf(" ");
+        StringBuffer sb = new StringBuffer(contents.substring(firstSpaceIndex));
+        String valueAsString = contents.substring(0, firstSpaceIndex);
+        int value = Integer.parseInt(valueAsString);
+
+        while (sb.length() > 0) {
+            Operator operator = getNextOperator(sb);
+            Integer operand = getNextValue(sb);
+            value = operator.applyOn(value, operand);
+        }
+
+        return value;
+    }
+
+    private Integer getNextValue(StringBuffer sb) {
+        // Drop first space
+        sb.delete(0, 1);
+
+        StringBuilder digits = new StringBuilder();
+        int index;
+        for (index = 0; index<sb.length(); index++) {
+            char c = sb.charAt(index);
+            if (Character.isDigit(c) || c == '-') {
+                digits.append(c);
+            } else {
+                break;
+            }
+        }
+        sb.delete(0, index);
+        return Integer.parseInt(digits.toString());
+    }
+
+    private Operator getNextOperator(StringBuffer sb) {
+        // Drop first space
+        sb.delete(0, 1);
+
+        for (Operator o : Operator.values()) {
+            if (sb.toString().startsWith(o.getEnglish())) {
+                sb.delete(0, o.getEnglish().length());
+                return o;
+            }
+        }
+        throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
+    }
+
+}
