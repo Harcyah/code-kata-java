fb6638b0557fe6e0a20396f30b9819d84c5277e3
Raph harcyah Bonnet <harcyah@gmail.com>
2017-01-28 18:14:44 +0100

    CodingBat map2

--- src/main/java/com/harcyah/kata/codingbat/map2/allSwap/AllSwap.java
+++ src/main/java/com/harcyah/kata/codingbat/map2/allSwap/AllSwap.java
@@ -1,5 +1,42 @@
 package com.harcyah.kata.codingbat.map2.allSwap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AllSwap {
+
+	public String[] allSwap(String[] strings) {
+		Map<Integer, Boolean> swapped = new HashMap<>();
+		for (int i = 0; i < strings.length; i++) {
+			swapped.put(i, false);
+		}
+
+		for (int i = 0; i < strings.length; i++) {
+			if (swapped.get(i)) {
+				continue;
+			}
+
+			String s = strings[i];
+			for (int j = 0; j < i; j++) {
+				if (swapped.get(j)) {
+					continue;
+				}
+
+				String needle = strings[j];
+
+				if (match(s, needle)) {
+					swapped.put(i, true);
+					swapped.put(j, true);
+					strings[i] = needle;
+					strings[j] = s;
+				}
+			}
+		}
+		return strings;
+	}
+
+	private boolean match(String s, String needle) {
+		return s.substring(0, 1).equals(needle.substring(0, 1));
+	}
 
 }
--- src/test/java/com/harcyah/kata/codingbat/map2/firstSwap/FirstSwapTest.java
+++ src/test/java/com/harcyah/kata/codingbat/map2/firstSwap/FirstSwapTest.java
@@ -1,5 +1,6 @@
 package com.harcyah.kata.codingbat.map2.firstSwap;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class FirstSwapTest {
@@ -7,6 +8,21 @@ public class FirstSwapTest {
 	@Test
 	public void testFirstSwap() {
 		FirstSwap firstSwap = new FirstSwap();
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "ab", "ac" })).isEqualTo(new String[] { "ac", "ab" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz" }))
+			.isEqualTo(new String[] { "ay", "by", "cy", "cx", "bx", "ax", "aaa", "azz" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "ax", "bx", "ay", "by", "ai", "aj", "bx", "by" }))
+			.isEqualTo(new String[] { "ay", "by", "ax", "bx", "ai", "aj", "bx", "by" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "ax", "bx", "cx", "ay", "cy", "aaa", "abb" }))
+			.isEqualTo(new String[] { "ay", "bx", "cy", "ax", "cx", "aaa", "abb" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "easy", "does", "it", "every", "ice", "eaten" }))
+			.isEqualTo(new String[] { "every", "does", "ice", "easy", "it", "eaten" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "list", "of", "words", "swims", "over", "lily", "water", "wait" }))
+			.isEqualTo(new String[] { "lily", "over", "water", "swims", "of", "list", "words", "wait" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "4", "8", "15", "16", "23", "42" })).isEqualTo(new String[] { "42", "8", "16", "15", "23", "4" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "aaa" })).isEqualTo(new String[] { "aaa" });
+		Assertions.assertThat(firstSwap.firstSwap(new String[] {})).isEqualTo(new String[] {});
+		Assertions.assertThat(firstSwap.firstSwap(new String[] { "a", "b", "c", "xx", "yy", "zz" })).isEqualTo(new String[] { "a", "b", "c", "xx", "yy", "zz" });
 	}
 
 }
