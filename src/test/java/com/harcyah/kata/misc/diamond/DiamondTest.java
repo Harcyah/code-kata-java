3b6cde485d2cbd882fb4333b5cb5bb0444483f3c
Raphaël Bonnet <rbonnet@betomorrow.com>
2017-02-10 14:13:28 +0100

    Diamond kata

---
+++ src/test/java/com/harcyah/kata/misc/diamond/DiamondTest.java
@@ -0,0 +1,46 @@
+package com.harcyah.kata.misc.diamond;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiamondTest {
+
+    @Test
+    public void diamondOfA() throws Exception {
+        Diamond diamond = new Diamond();
+        String s = diamond.diamondOf('A');
+        assertEquals("A", s);
+    }
+
+    @Test
+    public void diamondOfC() throws Exception {
+        Diamond diamond = new Diamond();
+        String s = diamond.diamondOf('C');
+        assertEquals(
+            "  A  " +
+            " B B " +
+            "C   C" +
+            " B B " +
+            "  A  ",
+            s);
+    }
+
+    @Test
+    public void diamondOfE() throws Exception {
+        Diamond diamond = new Diamond();
+        String s = diamond.diamondOf('E');
+        assertEquals(
+                "    A    " +
+                "   B B   " +
+                "  C   C  " +
+                " D     D " +
+                "E       E" +
+                " D     D " +
+                "  C   C  " +
+                "   B B   " +
+                "    A    ",
+                s);
+    }
+
+}
