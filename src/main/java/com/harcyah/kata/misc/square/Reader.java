2471973da6f3eff7305574ceebb83f503df52b40
Raphaël Bonnet <harcyah@gmail.com>
2020-05-07 20:18:03 +0200

    Remove commons-io dependency

--- src/main/java/com/harcyah/kata/misc/square/Reader.java
+++ src/main/java/com/harcyah/kata/misc/square/Reader.java
@@ -1,10 +1,10 @@
 package com.harcyah.kata.misc.square;
 
-import org.apache.commons.io.IOUtils;
+import com.google.common.io.Resources;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.TreeSet;
@@ -12,13 +12,13 @@ import java.util.TreeSet;
 public class Reader {
 
     public TreeSet<Point> read() throws IOException {
-        InputStream stream = getClass().getResourceAsStream("squares.txt");
-        List<String> lines = IOUtils.readLines(stream, StandardCharsets.UTF_8);
+        URL url = Resources.getResource("squares.txt");
+        List<String> lines = Resources.readLines(url, StandardCharsets.UTF_8);
         TreeSet<Point> points = new TreeSet<>();
         for (String line : lines) {
             Point point = new Point(
-                    Integer.parseInt(StringUtils.substringBefore(line, " ")),
-                    Integer.parseInt(StringUtils.substringAfter(line, " ")));
+                Integer.parseInt(StringUtils.substringBefore(line, " ")),
+                Integer.parseInt(StringUtils.substringAfter(line, " ")));
             points.add(point);
         }
         return points;
