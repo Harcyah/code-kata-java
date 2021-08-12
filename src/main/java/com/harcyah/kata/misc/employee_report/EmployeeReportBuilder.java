948d7542b85ce241794df3de813864254be3920b
Raphaël Bonnet <harcyah@gmail.com>
2021-08-12 17:13:26 +0200

    Add kata: EmployeeReportBuilder

---
+++ src/main/java/com/harcyah/kata/misc/employee_report/EmployeeReportBuilder.java
@@ -0,0 +1,25 @@
+package com.harcyah.kata.misc.employee_report;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class EmployeeReportBuilder {
+
+    private static final List<Employee> employees = List.of(
+        new Employee("Max", 17),
+        new Employee("Sepp", 18),
+        new Employee("Nina", 15),
+        new Employee("Mike", 51)
+    );
+
+    public List<Employee> getEmployees() {
+        return employees.stream()
+            .filter(e -> e.getAge() >= 18)
+            .sorted(Comparator.comparing(Employee::getName).reversed())
+            .map(it -> new Employee(it.getName().toUpperCase(Locale.ROOT), it.getAge()))
+            .collect(Collectors.toList());
+    }
+
+}
