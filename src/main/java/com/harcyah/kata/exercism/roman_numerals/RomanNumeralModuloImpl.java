5c28f49d5c380711c9c7c7518d263f762a2a1cd7
Raphaël Bonnet <harcyah@gmail.com>
2021-08-13 09:56:08 +0200

    Update RomanNumerals kata

--- src/main/java/com/harcyah/kata/exercism/roman_numerals/RomanNumeralModuloImpl.java
+++ src/main/java/com/harcyah/kata/exercism/roman_numerals/RomanNumeralModuloImpl.java
@@ -1,55 +1,50 @@
 package com.harcyah.kata.exercism.roman_numerals;
 
-public class RomanNumeral {
-
-    protected StringBuffer roman = new StringBuffer();
+public class RomanNumeralModuloImpl {
 
     private static final char[] ONES = new char[]{'I', 'X', 'C', 'M'};
     private static final char[] FIVES = new char[]{'V', 'L', 'D'};
 
-    public RomanNumeral(int input) {
+    public String get(int input) {
         if (input > 3000) {
             throw new IllegalArgumentException();
         }
 
-        int rank = 0;
-        String number = String.valueOf(input);
-        for (int i = number.length() - 1; i >= 0; i--) {
-            char numeral = number.charAt(i);
-            int value = Integer.parseInt("" + numeral);
-            roman.insert(0, getRomanForValue(value, rank));
-            rank++;
-        }
+        return getRomanForValue(input / 1000, 3)
+            + getRomanForValue(input % 1000 / 100, 2)
+            + getRomanForValue(input % 100 / 10, 1)
+            + getRomanForValue(input % 10, 0);
     }
 
-    private String getRomanForValue(int value, int rank) {
-        switch (value) {
+    private String getRomanForValue(int digit, int rank) {
+        switch (digit) {
             case 0:
+                return "";
             case 1:
+                return "" + ONES[rank];
             case 2:
+                return repeat(ONES[rank], 2);
             case 3:
-                return repeat(ONES[rank], value);
+                return repeat(ONES[rank], 3);
             case 4:
                 return "" + ONES[rank] + FIVES[rank];
             case 5:
                 return "" + FIVES[rank];
             case 6:
+                return FIVES[rank] + repeat(ONES[rank], 1);
             case 7:
+                return FIVES[rank] + repeat(ONES[rank], 2);
             case 8:
-                return FIVES[rank] + repeat(ONES[rank], value - 5);
+                return FIVES[rank] + repeat(ONES[rank], 3);
             case 9:
                 return "" + ONES[rank] + ONES[rank + 1];
             default:
-                return "";
+                throw new IllegalArgumentException();
         }
     }
 
     private String repeat(char c, int count) {
         return String.valueOf(c).repeat(count);
-    }
-
-    public String getRomanNumeral() {
-        return roman.toString();
     }
 
 }
