d18dc37f7e18b1fc2a974dd71ddabe3de81c1075
Raphaël Bonnet <harcyah@gmail.com>
2022-10-23 17:49:59 +0200

    Bump spotbugs to 4.7.3

--- src/main/java/com/harcyah/kata/codingbat/string3/mirror_ends/MirrorEnds.java
+++ src/main/java/com/harcyah/kata/codingbat/string3/mirror_ends/MirrorEnds.java
@@ -3,18 +3,18 @@ package com.harcyah.kata.codingbat.string3.mirror_ends;
 public class MirrorEnds {
 
     public String mirrorEnds(String source) {
-        String mirror = "";
+        StringBuilder sb = new StringBuilder("");
         for (int i = 0; i < source.length(); i++) {
             int end = source.length() - 1 - i;
             char a = source.charAt(i);
             char b = source.charAt(end);
             if (a == b) {
-                mirror += a;
+                sb.append(a);
             } else {
                 break;
             }
         }
-        return mirror;
+        return sb.toString();
     }
 
 }
