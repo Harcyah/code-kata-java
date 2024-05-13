672e2da645433216251feb183dda51abd8ee57eb
Raphaël Bonnet <harcyah@gmail.com>
2024-05-13 21:26:13 +0200

    Refactoring : Make use of String.replace method

--- src/main/java/com/harcyah/kata/codingbat/functional1/NoX.java
+++ src/main/java/com/harcyah/kata/codingbat/functional1/NoX.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class NoX {
 
     public List<String> noX(List<String> strings) {
-        return strings.stream().map(x -> x.replaceAll("x", "")).toList();
+        return strings.stream().map(x -> x.replace("x", "")).toList();
     }
 
 }
