2f55ecdfcca385934f6b185ec49f60a6257cc873
Raphaël Bonnet <harcyah@gmail.com>
2024-10-11 21:13:23 +0200

    Refactoring : Rewrite BobThere without regex

--- src/main/java/com/harcyah/kata/codingbat/string2/bob_there/BobThere.java
+++ src/main/java/com/harcyah/kata/codingbat/string2/bob_there/BobThere.java
@@ -2,8 +2,20 @@ package com.harcyah.kata.codingbat.string2.bob_there;
 
 public class BobThere {
 
-    public boolean bobThere(String str) {
-        return str.matches(".*b.b.*");
+    public boolean bobThere(String it) {
+        if (it == null || it.length() < 3) {
+            return false;
+        }
+
+        for (int i = 0; i < it.length() - 2; i++) {
+            char left = it.charAt(i);
+            char right = it.charAt(i + 2);
+            if (left == right && left == 'b') {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
