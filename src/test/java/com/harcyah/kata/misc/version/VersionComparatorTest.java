1743db2d3ae3924db1175143450c8dd759037068
Raphaël Bonnet <harcyah@gmail.com>
2025-07-17 21:35:22 +0200

    Refactoring : Make use of isZero()/isNegative()/isPositive() assertions

--- src/test/java/com/harcyah/kata/misc/version/VersionComparatorTest.java
+++ src/test/java/com/harcyah/kata/misc/version/VersionComparatorTest.java
@@ -2,66 +2,66 @@ package com.harcyah.kata.misc.version;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class VersionComparatorTest {
 
-    private VersionComparator c = new VersionComparator();
+    private final VersionComparator sut = new VersionComparator();
 
     @Test
     void testNullOrder() {
-        assertTrue(c.compare(null, "2") < 0);
-        assertTrue(c.compare("2", null) > 0);
-        assertTrue(c.compare(null, null) == 0);
+        assertThat(sut.compare(null, "2")).isNegative();
+        assertThat(sut.compare("2", null)).isPositive();
+        assertThat(sut.compare(null, null)).isZero();
     }
 
     @Test
     void testBasicNumericOrder() {
-        assertTrue(c.compare("1", "2") < 0);
-        assertTrue(c.compare("2", "1") > 0);
-        assertTrue(c.compare("1", "1") == 0);
-        assertTrue(c.compare("1", "12") < 0);
-        assertTrue(c.compare("2", "12") < 0);
+        assertThat(sut.compare("1", "2")).isNegative();
+        assertThat(sut.compare("2", "1")).isPositive();
+        assertThat(sut.compare("1", "1")).isZero();
+        assertThat(sut.compare("1", "12")).isNegative();
+        assertThat(sut.compare("2", "12")).isNegative();
     }
 
     @Test
     void testSubversionNumericOrder() {
-        assertTrue(c.compare("1.1", "1.2") < 0);
-        assertTrue(c.compare("1.2", "1.1") > 0);
-        assertTrue(c.compare("1.1", "1.1") == 0);
+        assertThat(sut.compare("1.1", "1.2")).isNegative();
+        assertThat(sut.compare("1.2", "1.1")).isPositive();
+        assertThat(sut.compare("1.1", "1.1")).isZero();
 
-        assertTrue(c.compare("1.1", "2.1") < 0);
-        assertTrue(c.compare("1.2", "2.1") < 0);
-        assertTrue(c.compare("1.12", "1.1") > 0);
+        assertThat(sut.compare("1.1", "2.1")).isNegative();
+        assertThat(sut.compare("1.2", "2.1")).isNegative();
+        assertThat(sut.compare("1.12", "1.1")).isPositive();
     }
 
     @Test
     void testSubversionLengthOrder() {
-        assertTrue(c.compare("1.1", "1.1.1") < 0);
-        assertTrue(c.compare("1.2", "1.1.1") > 0);
+        assertThat(sut.compare("1.1", "1.1.1")).isNegative();
+        assertThat(sut.compare("1.2", "1.1.1")).isPositive();
     }
 
     @Test
     void testNumericLessThanNonNumeric() {
-        assertTrue(c.compare("1.1", "1.a") < 0);
-        assertTrue(c.compare("1.2", "1.1a") < 0);
-        assertTrue(c.compare("1.2", "1.-") < 0);
-        assertTrue(c.compare("1.2", "1. ") < 0);
+        assertThat(sut.compare("1.1", "1.a")).isNegative();
+        assertThat(sut.compare("1.2", "1.1a")).isNegative();
+        assertThat(sut.compare("1.2", "1.-")).isNegative();
+        assertThat(sut.compare("1.2", "1. ")).isNegative();
     }
 
     @Test
     void testNonNumericOrder() {
-        assertTrue(c.compare("a", "b") < 0);
-        assertTrue(c.compare("B", "a") < 0);
-        assertTrue(c.compare("a", "ab") < 0);
-        assertTrue(c.compare("1.1a", "1.1b") < 0);
+        assertThat(sut.compare("a", "b")).isNegative();
+        assertThat(sut.compare("B", "a")).isNegative();
+        assertThat(sut.compare("a", "ab")).isNegative();
+        assertThat(sut.compare("1.1a", "1.1b")).isNegative();
     }
 
     @Test
     void testMixedNumericNonNumeric() {
-        assertTrue(c.compare("2.1b", "3.1a") < 0);
-        assertTrue(c.compare("3.1.3", "3.1a.2beta") < 0);
-        assertTrue(c.compare("1.1a.2", "1.1b.1") < 0);
+        assertThat(sut.compare("2.1b", "3.1a")).isNegative();
+        assertThat(sut.compare("3.1.3", "3.1a.2beta")).isNegative();
+        assertThat(sut.compare("1.1a.2", "1.1b.1")).isNegative();
     }
 
 }
