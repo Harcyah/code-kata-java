8e90ceec92540abb4259ba2059cc3ccff2b38fd5
Raphaël Bonnet <harcyah@gmail.com>
2025-07-18 22:02:42 +0200

    Refactoring : Make use of text blocks

--- src/test/java/com/harcyah/kata/exercism/food_chain/FoodChainTest.java
+++ src/test/java/com/harcyah/kata/exercism/food_chain/FoodChainTest.java
@@ -1,194 +1,164 @@
 package com.harcyah.kata.exercism.food_chain;
 
-import org.junit.jupiter.api.BeforeEach;
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FoodChainTest {
-    private FoodChain foodChain;
 
-    @BeforeEach
-    void setup() {
-        foodChain = new FoodChain();
+    private final FoodChain sut = new FoodChain();
+
+    @ParameterizedTest
+    @EnumSource(FoodChainFixture.class)
+    void verse(FoodChainFixture fixture) {
+        // Given
+        int verse = fixture.verse;
+        String expected = fixture.expected;
+
+        assertEquals(expected, sut.verse(verse));
     }
 
-    @Test
-    void fly() {
-        int verse = 1;
-        String expected = "I know an old lady who swallowed a fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
+    @AllArgsConstructor
+    private enum FoodChainFixture {
+        FLY(1, """
+            I know an old lady who swallowed a fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        SPIDER(2, """
+            I know an old lady who swallowed a spider.
+            It wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        BIRD(3, """
+            I know an old lady who swallowed a bird.
+            How absurd to swallow a bird!
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        CAT(4, """
+            I know an old lady who swallowed a cat.
+            Imagine that, to swallow a cat!
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        DOG(5, """
+            I know an old lady who swallowed a dog.
+            What a hog, to swallow a dog!
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        GOAT(6, """
+            I know an old lady who swallowed a goat.
+            Just opened her throat and swallowed a goat!
+            She swallowed the goat to catch the dog.
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        COW(7, """
+            I know an old lady who swallowed a cow.
+            I don't know how she swallowed a cow!
+            She swallowed the cow to catch the goat.
+            She swallowed the goat to catch the dog.
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die."""),
+        HORSE(8, """
+            I know an old lady who swallowed a horse.
+            She's dead, of course!""");
 
-        assertEquals(expected, foodChain.verse(verse));
-    }
+        private final int verse;
+        private final String expected;
 
-    @Test
-    void spider() {
-        int verse = 2;
-        String expected = "I know an old lady who swallowed a spider.\n" +
-                "It wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void bird() {
-        int verse = 3;
-        String expected = "I know an old lady who swallowed a bird.\n" +
-                "How absurd to swallow a bird!\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void cat() {
-        int verse = 4;
-        String expected = "I know an old lady who swallowed a cat.\n" +
-                "Imagine that, to swallow a cat!\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void dog() {
-        int verse = 5;
-        String expected = "I know an old lady who swallowed a dog.\n" +
-                "What a hog, to swallow a dog!\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void goat() {
-        int verse = 6;
-        String expected = "I know an old lady who swallowed a goat.\n" +
-                "Just opened her throat and swallowed a goat!\n" +
-                "She swallowed the goat to catch the dog.\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void cow() {
-        int verse = 7;
-        String expected = "I know an old lady who swallowed a cow.\n" +
-                "I don't know how she swallowed a cow!\n" +
-                "She swallowed the cow to catch the goat.\n" +
-                "She swallowed the goat to catch the dog.\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
-
-        assertEquals(expected, foodChain.verse(verse));
-    }
-
-    @Test
-    void horse() {
-        int verse = 8;
-        String expected = "I know an old lady who swallowed a horse.\n" +
-                "She's dead, of course!";
-
-        assertEquals(expected, foodChain.verse(verse));
     }
 
     @Test
     void multipleVerses() {
         int startVerse = 1;
         int endVerse = 3;
-        String expected = "I know an old lady who swallowed a fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a spider.\n" +
-                "It wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a bird.\n" +
-                "How absurd to swallow a bird!\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.";
+        String expected = """
+            I know an old lady who swallowed a fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
 
-        assertEquals(expected, foodChain.verses(startVerse, endVerse));
+            I know an old lady who swallowed a spider.
+            It wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a bird.
+            How absurd to swallow a bird!
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.""";
+
+        assertEquals(expected, sut.verses(startVerse, endVerse));
     }
 
     @Test
     void wholeSong() {
         int startVerse = 1;
         int endVerse = 8;
-        String expected = "I know an old lady who swallowed a fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a spider.\n" +
-                "It wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a bird.\n" +
-                "How absurd to swallow a bird!\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a cat.\n" +
-                "Imagine that, to swallow a cat!\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a dog.\n" +
-                "What a hog, to swallow a dog!\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a goat.\n" +
-                "Just opened her throat and swallowed a goat!\n" +
-                "She swallowed the goat to catch the dog.\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a cow.\n" +
-                "I don't know how she swallowed a cow!\n" +
-                "She swallowed the cow to catch the goat.\n" +
-                "She swallowed the goat to catch the dog.\n" +
-                "She swallowed the dog to catch the cat.\n" +
-                "She swallowed the cat to catch the bird.\n" +
-                "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly.\n" +
-                "I don't know why she swallowed the fly. Perhaps she'll die.\n" +
-                "\n" +
-                "I know an old lady who swallowed a horse.\n" +
-                "She's dead, of course!";
+        String expected = """
+            I know an old lady who swallowed a fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
 
-        assertEquals(expected, foodChain.verses(startVerse, endVerse));
+            I know an old lady who swallowed a spider.
+            It wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a bird.
+            How absurd to swallow a bird!
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a cat.
+            Imagine that, to swallow a cat!
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a dog.
+            What a hog, to swallow a dog!
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a goat.
+            Just opened her throat and swallowed a goat!
+            She swallowed the goat to catch the dog.
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a cow.
+            I don't know how she swallowed a cow!
+            She swallowed the cow to catch the goat.
+            She swallowed the goat to catch the dog.
+            She swallowed the dog to catch the cat.
+            She swallowed the cat to catch the bird.
+            She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+            She swallowed the spider to catch the fly.
+            I don't know why she swallowed the fly. Perhaps she'll die.
+
+            I know an old lady who swallowed a horse.
+            She's dead, of course!""";
+
+        assertEquals(expected, sut.verses(startVerse, endVerse));
     }
 
 }
